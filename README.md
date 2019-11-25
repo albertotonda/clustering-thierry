98b210d02725f612fde5c2e8d8b42831bd56e5be
albertotonda <alberto.tonda@gmail.com>
2019-11-25 14:18:39 +0100

    Added TODO list with a few notes. It would be useful to pre-compute the distance matrix.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ In the beginning, data was 183 .csv files + one file with the meta-data.
 - Choose a certain number of clusters
 - Plot the time series in each cluster
 
+TODO: pre-compute distance matrix (DTW is really slow)
+
 ## 2019-11-24
 Look up Dynamic Time Warping, https://towardsdatascience.com/time-series-hierarchical-clustering-using-dynamic-time-warping-in-python-c8c9edf2fda5
 
--- src/experiment-tslearn.py
+++ src/experiment-tslearn.py
@@ -5,8 +5,10 @@ import pandas as pd
 import scipy.cluster.hierarchy as shc
 
 from dtaidistance import dtw
+from sklearn.cluster import AgglomerativeClustering
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 from tslearn import metrics
+from tslearn.clustering import TimeSeriesKMeans
 
 # prepare dataset with all time series
 print("Loading/creating dataset...")
@@ -48,9 +50,17 @@ for index, row in df.iterrows() :
     time_series.append( df_experiment.iloc[:,2].values.tolist() )
     time_series_names.append( row[column_file].split('.')[0] )
 
+for i in range(0, len(time_series)) :
+    print("Time series (unscaled) #%d has %d points" % (i, len(time_series[i])))
+
 print("Preparing data...")
-scaler = TimeSeriesScalerMeanVariance(mu=0., std=1.) # rescale time series
-time_series_scaled = scaler.fit_transform(time_series)
+# unfortunately, this scalingmethod requires time series of the same size
+#scaler = TimeSeriesScalerMeanVariance(mu=0., std=1.) # rescale time series
+#time_series_scaled = scaler.fit_transform(time_series)
+time_series_scaled = time_series
+
+for i in range(0, len(time_series_scaled)) :
+    print("Time series (scaled) #%d has %d points" % (i, len(time_series_scaled[i])))
 
 print("Computing Dynamic Time Warping distance matrix...")
 distance_matrix = np.zeros((n_time_series, n_time_series))
@@ -67,8 +77,18 @@ for i in range(0, n_time_series) :
             distance_matrix[i, j] = distance
         
 # now, let's call our clustering algorithms, with a pre-computed distance matrix
-plt.figure(figsize=(10, 7))
-plt.title("Dendrograms")
-dend = shc.dendrogram(shc.linkage(time_series_scaled, method='ward', metric=lambda u, v : distance_matrix(u, v)))
-plt.axhline(y=cutoff_distance, color='r', linestyle='--')
-plt.savefig( os.path.join(folder_name, "hierarchical-dendogram.pdf") )
+if False : # commented out, at the moment
+    plt.figure(figsize=(10, 7))
+    plt.title("Dendrograms")
+    dend = shc.dendrogram(shc.linkage(time_series_scaled, method='ward', metric=lambda u, v : distance_matrix(time_series_scaled.index(u), time_series_scaled.index(v))))
+    plt.axhline(y=cutoff_distance, color='r', linestyle='--')
+    plt.savefig( os.path.join(folder_name, "hierarchical-dendogram.pdf") )
+
+print("Performing agglomerative clustering...")
+n_clusters = 10
+clusterer = TimeSeriesKMeans(n_clusters=n_clusters, metric='dtw', random_state=0)
+clusterer.fit_predict(time_series_scaled)
+
+print("Clusters:")
+for c in range(0, clusterer.labels_.max() + 1) :
+    print("- Cluster #%d has %d time series" % (c, sum(clusterer.labels_ == c)))
